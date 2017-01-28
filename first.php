166b884b6c7665834dae09b1714c1187cc58d1c4
RobinZeininger <zeininger.robin@gmail.com>
2017-01-28 18:26:39 +0100

    Bugfixing landing-page

---
+++ first.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+
+
+</head>
+<body>
+<img id="earth" src="media/earth.jpg" alt="earth">
+
+<div class="fadeIn animated">
+
+    <h2>PlaneWatch</h2>
+
+<h3>See flights around the world, based on cities and airlines.</h3>
+</div>
+
+
+<div id="buttonid" class="fadeInDown animated">
+    <a href="index.php"><button>Let's Go</button></a>
+</div>
+
+<h4>made with ♥ by Andreas Eisende, Boris Sedlak, Bernhard Hartmann & Robin Zeininger</h4>
+</body>
+</html>
